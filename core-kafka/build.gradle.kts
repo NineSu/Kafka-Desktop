a03cc9e1304dc2e09ba2508b0891187ccf4bf76c
NineSu <ninesunice@qq.com>
2026-05-27 18:09:11 +0800

    build: add core-kafka module skeleton

---
+++ core-kafka/build.gradle.kts
@@ -0,0 +1,11 @@
+dependencies {
+    implementation(libs.kafka.clients)
+    implementation(libs.slf4j.api)
+
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.kotest.assertions)
+    testImplementation(libs.testcontainers.kafka)
+    testImplementation(libs.testcontainers.junit)
+    testImplementation(libs.mockk)
+    testRuntimeOnly(libs.logback.classic)
+}
